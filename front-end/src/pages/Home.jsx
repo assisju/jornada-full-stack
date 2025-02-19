0a4d8665129e8a6f5f6f1cac9d11abf2aa6c58b9
assisju <juli-assis@hotmail.com>
2025-02-19 14:46:00 -0300

    finalizamos o projeto

---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Main from "../Components/Main";
+
+const Home = () => {
+  return <Main />;
+};
+
+export default Home;
---
+++ front-end/src/pages/Songs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Main from "../Components/Main";
+
+const Songs = () => {
+  return <Main type="songs" />;
+};
+
+export default Songs;
